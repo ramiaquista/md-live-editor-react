a069c2f17f884d24834d0ecf973d9a00c6e97fce
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-04-20 20:04:27 +0200

    Add basic react app config with tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./navBar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-black h-screen">
+      < NavBar />
+      <h1 className="text-3xl font-bold text-white">
+        MarkDown Live Editor
+      </h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Configure your color palette here
+        'custom-gray': '#2c2d30',
+      },
+    },
+  },
+  plugins: [],
+}
